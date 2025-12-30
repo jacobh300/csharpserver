2a996e7267fb2c5384d43d8f76ee015fded47c22
Jacob <jacob.hyatt@clipwiregames.com>
2025-12-30 13:19:53 -0500

    remove velocity set

--- Game.cs
+++ Game.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Numerics;
 using SpacetimeDB;
 
-
 /// <summary>
 /// Handles main game logic and state, mainly the tick loop.
 /// </summary>
@@ -20,7 +19,8 @@ public class Game
         // For each player, get all their player inputs and update their positions accordingly.
         foreach (var transform in ctx.Db.player_transform.Iter())
         {
-            transform.velocity = new DbVector3(0,0,0);
+
+            //transform.velocity = new DbVector3(0,0,0);
             IEnumerable<Module.PlayerInputRow> inputRows = ctx.Db.player_input.playerIndex.Filter(transform.player);
             foreach (var inputRow in inputRows)
             {
@@ -28,7 +28,7 @@ public class Game
                 transform.position.x += inputRow.input.x * transform.moveSpeed;
                 transform.position.y += 0;
                 transform.position.z += inputRow.input.y * transform.moveSpeed;
-                transform.velocity = new DbVector3(inputRow.input.x * transform.moveSpeed, 0, inputRow.input.y * transform.moveSpeed);
+                //transform.velocity = new DbVector3(inputRow.input.x * transform.moveSpeed, 0, inputRow.input.y * transform.moveSpeed);
                 transform.yaw = inputRow.yaw;
                 transform.tick = gameTick.tick; 
                 transform.sequence = inputRow.sequence;
@@ -36,8 +36,7 @@ public class Game
                 //Remove processed input
                 ctx.Db.player_input.id.Delete(inputRow.id);
             }
-             ctx.Db.player_transform.player.Update(transform);
-
+            ctx.Db.player_transform.player.Update(transform);
         }
 
         // Update the game tick schedule
